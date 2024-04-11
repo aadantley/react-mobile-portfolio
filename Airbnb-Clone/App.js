c23216565bd13e5a0270de7c6a3e56467bf44f78
Aaron Dantley <aadantley@gmail.com>
2024-04-10 23:28:14 -0400

    airbnb clone:
    - added map view
    - added fake location data
    - added react-native-maps lib

--- Airbnb-Clone/App.js
+++ Airbnb-Clone/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 38.9072,
+          longitude: -77.0369,
+          latitudeDelta: 0.0052,
+          longitudeDelta: 0.0052,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 38.9072, longitude: -77.0369 }}
+          title="Coffee Shop"
+          description="Oystried"
+        />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
