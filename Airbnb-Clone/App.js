e25b412409fe910994cba281060bd42539c9fe01
Aaron Dantley <aadantley@gmail.com>
2024-04-14 11:54:51 -0400

    updates:
    - reverting back to previous working version
    - going to make bottom sheet list as a subcomponent to try various implementations out

--- Airbnb-Clone/App.js
+++ Airbnb-Clone/App.js
@@ -1,10 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, StyleSheet } from "react-native";
 import CustomMarker from "./components/CustomMarker";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import React, { useMemo, useState, useRef, useCallback } from "react";
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import MapView from "react-native-maps";
+import React, { useState } from "react";
 
 //local
 import apartments from "./data/apartments.json";
@@ -14,46 +12,34 @@ export default function App() {
   // Apartment Detail Card
   const [selectedApartment, setSelectedApartment] = useState(null);
 
-  // Bottom Sheet
-  const snapPoints = useMemo(() => ["25%", "50%"], []);
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View>
-        <StatusBar style="auto" />
+    <View>
+      <StatusBar style="auto" />
 
-        {/* Map View of Region */}
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 38.9072,
-            longitude: -77.0369,
-            latitudeDelta: 0.0052,
-            longitudeDelta: 0.0052,
-          }}
-        >
-          {/* Markers per Item */}
-          {apartments.map((apartment) => (
-            <CustomMarker
-              apartment={apartment}
-              key={apartment.id}
-              onPress={() => setSelectedApartment(apartment)}
-            />
-          ))}
-        </MapView>
-        {/* Apartment Detail Card */}
-        {selectedApartment && <ApartmentDetail apartment={selectedApartment} />}
+      {/* Map View of Region */}
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 38.9072,
+          longitude: -77.0369,
+          latitudeDelta: 0.0052,
+          longitudeDelta: 0.0052,
+        }}
+      >
+        {/* Markers per Item */}
+        {apartments.map((apartment) => (
+          <CustomMarker
+            apartment={apartment}
+            key={apartment.id}
+            onPress={() => setSelectedApartment(apartment)}
+          />
+        ))}
+      </MapView>
+      {/* Apartment Detail Card */}
+      {selectedApartment && <ApartmentDetail apartment={selectedApartment} />}
 
-        {/* Bottom Sheet */}
-        <View style={styles.container}>
-          <BottomSheet index={1} snapPoints={snapPoints}>
-            <BottomSheetView style={styles.contentContainer}>
-              <Text>Awesome 🎉</Text>
-            </BottomSheetView>
-          </BottomSheet>
-        </View>
-      </View>
-    </GestureHandlerRootView>
+      {/* Bottom Sheet */}
+    </View>
   );
 }
 
