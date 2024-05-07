fe5b4ef00cf695951bb647fe57253094e9f57bbd
Aaron Dantley <aadantley@gmail.com>
2024-05-07 17:42:34 -0400

    updates:
    - adding screen components
    - package update for reanimated 3.11.0 --> 3.10.0

--- bottomNavIconBar/screens/DetailScreen.js
+++ bottomNavIconBar/screens/DetailScreen.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function DetailScreen() {
   return (
     <View>
-      <Text>DetailScreen</Text>
+      <Text onPress={() => navigation.navigate("Home")}>DetailScreen</Text>
     </View>
-  )
+  );
 }
