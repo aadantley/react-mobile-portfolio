11085758cc7ec20c7d98ea752791fbed2f8fdb67
Aaron Dantley <aadantley@gmail.com>
2024-05-08 18:18:28 -0400

    restoring blank App.js to follow different tutorial

--- bottomNavIconBar/navigation/MainContainer.js
+++ bottomNavIconBar/navigation/MainContainer.js
@@ -1,14 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-vector-icons/FontAwesome";
 
+// screens
+import HomeScreen from "./screens/HomeScreen";
+import DetailScreen from "./screens/DetailScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+// Screen names
+
+const homeName = "Home";
+const detailsName = "Details";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
 
 export default function MainContainer() {
   return (
-    <View>
-      <Text>MainContainer</Text>
-    </View>
-  )
+    <Tab.Navigator>
+      <Tab.Screen 
+      name="Home" 
+      component={HomeScreen}
+      options = {{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({color, size}) => (
+          <Icon />
+        )
+      }}
+     
+      />
+      <Tab.Screen name="Details" component={DetailScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
 }
