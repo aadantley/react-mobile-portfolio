96d46313bcea98582d423ff803d6769ee346da51
Aaron Dantley <aadantley@gmail.com>
2024-05-10 09:27:54 -0400

    updates:
    - adding material icons
    - adding home and profile screens

--- bottomNavIconBar/App.js
+++ bottomNavIconBar/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+//screens
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>Hola</Text>
-      <StatusBar />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
