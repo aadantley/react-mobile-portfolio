a44b9e9a430a35f478bd7969cfc715ae9c504cb8
Aaron Dantley <aadantley@gmail.com>
2024-07-06 23:57:21 -0400

    adding details sceren; adding stack navigation options

---
+++ WorkoutTrackerClone/app/[name].js
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { Stack } from "expo-router";
+
+// data
+import exercises from "../assets/data/exercises.json";
+
+export default function ExerciseDetailScreen() {
+  const params = useLocalSearchParams();
+
+  const exercise = exercises.find((item) => item.name === params.name);
+
+  if (!exercise) {
+    return <Text>Exercise Not Found</Text>;
+  }
+
+  console.log(params);
+  return (
+    <View>
+      <Stack.Screen options={{ title: "Hello" }} />
+      <Text>Exercise Details: {exercise.name}</Text>
+      <Text>Muscle: {exercise.muscle}</Text>
+      <Text>Equipment: {exercise.equipment}</Text>
+      <Text>Type: {exercise.type}</Text>
+    </View>
+  );
+}
--- WorkoutTrackerClone/components/ExerciseListItem/ExerciseListItem.js
+++ WorkoutTrackerClone/components/ExerciseListItem/ExerciseListItem.js
@@ -1,15 +1,18 @@
-import { View, Text } from "react-native";
+import { Text, Pressable } from "react-native";
+import { Link } from "expo-router";
 import React from "react";
 import styles from "./styles";
 
 export default function ExerciseListItem({ item }) {
   return (
-    <View style={styles.exerciseContainer}>
-      <Text style={styles.exerciseName}>{item.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        <Text style={styles.subText}>{item.muscle}</Text> |{" "}
-        <Text style={styles.subText}>{item.equipment}</Text>
-      </Text>
-    </View>
+    <Link href={`/${item.name}`} asChild>
+      <Pressable style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{item.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          <Text style={styles.subText}>{item.muscle}</Text> |{" "}
+          <Text style={styles.subText}>{item.equipment}</Text>
+        </Text>
+      </Pressable>
+    </Link>
   );
 }
