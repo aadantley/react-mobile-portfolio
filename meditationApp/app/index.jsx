bced0ad02aa04d1d71a8ea95c1b117eaac5b38c5
Aaron Dantley <aadantley@gmail.com>
2024-11-25 23:18:51 -0500

    ignoring use of nativewind for now

--- meditationApp/app/index.jsx
+++ meditationApp/app/index.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 export default function Home() {
   return (
-    <View className="flex-1 align-middle justify-center">
+    <View style={styles.container}>
       <Text>Hello World</Text>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
