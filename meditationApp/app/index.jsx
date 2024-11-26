50240631afc0ee1d72ce4b0962ff7cd75fcb57e7
Aaron Dantley <aadantley@gmail.com>
2024-11-25 23:11:56 -0500

    adding babel config

--- meditationApp/app/index.jsx
+++ meditationApp/app/index.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 align-middle justify-center">
       <Text>Hello World</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
